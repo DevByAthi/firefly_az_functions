7a0543037cc76c8536f05f18807f5c0ab4ce8527
Athreya Murali <34428511+DevByAthi@users.noreply.github.com>
2021-04-14 23:26:36 -0400

    More playing around with IoT Hub triggering, successfully picked up device data. Need to figure out output

--- FireFlyFunctions/iothub_to_blob/__init__.py
+++ FireFlyFunctions/iothub_to_blob/__init__.py
@@ -1,14 +1,15 @@
 from typing import List
 import logging
 
+import azure.functions
 import azure.functions as func
 
 
-def main(events: List[func.EventHubEvent]) -> func.HttpResponse:
-    logging.info("RECEIVED")
-    for event in events:
-        logging.info('Python EventHub trigger processed an event: %s',
-                     event.get_body().decode('utf-8'))
+def main(events: List[func.EventHubEvent], context: azure.functions.Context) -> func.HttpResponse:
+    logging.info("RECEIVED {} events for {}".format(len(events), context.invocation_id))
+    for i in range(len(events)):
+        event = events[i]
+        logging.info("Event #{}: {}".format(i+1, event.get_body().__str__()))
     if len(events) > 0:
-        return func.HttpResponse("SUCCESS {}".format(events[0].get_body()), status_code=200)
+        return func.HttpResponse("SUCCESS", status_code=200)
     return func.HttpResponse("FAILURE", status_code=400)
