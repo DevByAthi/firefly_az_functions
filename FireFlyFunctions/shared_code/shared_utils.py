83ac07af636c5801b480ac4c39464138a5028bf2
Athreya Murali <34428511+DevByAthi@users.noreply.github.com>
2021-04-20 01:24:50 -0400

    Removed redundant conditional in return statement; if `d` is a Feature, then it must have a properties key

--- FireFlyFunctions/shared_code/shared_utils.py
+++ FireFlyFunctions/shared_code/shared_utils.py
@@ -4,6 +4,5 @@ import geojson
 def meets_device_format(d, is_sensor=True):
     expected_device_type = 'sensor' if is_sensor else 'actuator'
     return isinstance(d, geojson.Feature) \
-           and 'properties' in d \
            and 'device_type' in d['properties'] \
            and d['properties']['device_type'] == expected_device_type
