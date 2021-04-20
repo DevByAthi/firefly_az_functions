c67b4b9c6b436b9f0c0eb15f35b3a6e0a6d975cd
Athreya Murali <34428511+DevByAthi@users.noreply.github.com>
2021-04-20 01:25:23 -0400

    Adding test logging

--- FireFlyFunctions/handle_actuator/__init__.py
+++ FireFlyFunctions/handle_actuator/__init__.py
@@ -1,5 +1,7 @@
 import json.decoder
+import os
 from typing import List
+import logging
 import geojson
 
 import azure.functions as func
@@ -7,20 +9,27 @@ import azure.functions as func
 from ..shared_code.shared_utils import meets_device_format
 
 
-def main(events: List[func.EventHubEvent], out: func.Out[func.Document]):
+def main(events: List[func.EventHubEvent], msg: func.Out[func.Document]):
     for event in events:
         decoded_message = event.get_body().decode('utf-8')
-        # logging.info('Python EventHub trigger processed an event: %s',
-        #                 event.get_body().decode('utf-8'))
 
         # dictionary obtained from message
         decoded_dict = None
+
+        logging.info("TEST 1")
+
         try:
             decoded_dict = geojson.loads(decoded_message)
-        except json.decoder.JSONDecodeError as err:
+        except json.decoder.JSONDecodeError:
             continue
+        logging.info("TEST 2: Successfully converted to dict without error")
 
         # Ensure that GeoJSON format is met, with requirement that event be from an "actuator", i.e. drone
-        if not meets_device_format(decoded_dict, is_sensor=False):
+        if decoded_dict is None or not meets_device_format(decoded_dict, is_sensor=False):
             continue
 
+        logging.info("TEST 3: Meets defined GeoJSON format")
+
+        logging.info('Python EventHub trigger processed an event: %s', decoded_message)
+        msg.set(func.Document.from_dict(decoded_dict))
+        logging.info("TEST 4: Successfully sent")
