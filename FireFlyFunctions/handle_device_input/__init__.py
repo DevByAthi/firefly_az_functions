9406e6c7cbc7ff1581d4b99ad2b23bb619697713
Athreya Murali <34428511+DevByAthi@users.noreply.github.com>
2021-04-22 02:02:59 -0400

    Renamed `handle_sensor` to more generic `handle_device_input`

--- FireFlyFunctions/handle_device_input/__init__.py
+++ FireFlyFunctions/handle_device_input/__init__.py
@@ -5,6 +5,10 @@ import azure.functions as func
 
 
 def main(events: List[func.EventHubEvent]):
+    """
+    Takes sensor input, extract
+    :param events:
+    """
     for event in events:
         logging.info('Python EventHub trigger processed an event: %s',
                         event.get_body().decode('utf-8'))
