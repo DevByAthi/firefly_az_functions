0f5b1be97dfc3fad53e16cda86fc3615d5fe72ad
Athreya Murali <34428511+DevByAthi@users.noreply.github.com>
2021-04-23 01:51:21 -0400

    Added debug string returned to IoT Hub. Hopefully won't result in infinite loop!!

--- FireFlyFunctions/parse_actuator_data/__init__.py
+++ FireFlyFunctions/parse_actuator_data/__init__.py
@@ -1,8 +1,16 @@
 import logging
+import os
 
 import azure.functions as func
+from azure.cosmos import CosmosClient
+
+CLIENT = CosmosClient.from_connection_string(os.environ['AzureCosmosDBConnectionString'])
 
 
 def main(documents: func.DocumentList) -> str:
     if documents:
         logging.info('Document id: %s', documents[0]['id'])
+        logging.info("Documents List length: {}".format(len(documents)))
+        if 'geometry' not in documents[0]:
+            return "FAILED TO RETRIEVE CORRECT DOCUMENT"
+        return str(documents[0]['geometry']).replace("'", '"')
