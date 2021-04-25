65ab83991492a6886a47ae6850ca67f54de5aecb
Athreya Murali <34428511+DevByAthi@users.noreply.github.com>
2021-04-25 04:13:40 -0400

    Added method calls for running Welzl's Algorithm and Vincenty's Direct Method to obtain destination points for drone

--- FireFlyFunctions/parse_actuator_data/__init__.py
+++ FireFlyFunctions/parse_actuator_data/__init__.py
@@ -1,19 +1,47 @@
+"""
+parse_actuator_data.py
+
+Query for sensor readings at a regular interval (may change trigger). Use sensor readings that detect a wildfire to
+estimate where the fire is After parsing sensor data to obtain coordinates of triggered sensors, apply Welzl's
+Algorithm to finding the minimum enclosing circle to estimate the wildfire's outer boundary
+
+After running Welzl's Algorithm, return a fixed number of points on the circumference, spaced at regular intervals.
+These coordinates will form the flight path of the drone, and they will be sent via the Azure IoT Hub direct method
+protocol
+
+Azure Function made by Athreya Murali
+Welzl's Algorithm implementation made by Karmela Flynn
+Vincenty Direct Method implementation made by Paul Kennedy and Jim Leven
+
+Last Modified: 25 April 2021, 4:09 AM EDT
+"""
+
 import logging
 import os
 from datetime import datetime, timedelta
 
 import azure.functions as func
 import geojson
-import pygeodesy as pyg
 from azure.cosmos import CosmosClient
 
 import numpy as np
 
 from ..shared_code.shared_utils import dict_to_str
 from ..shared_code.welzl import welzl, NSphere
+from ..shared_code.vincenty import vincentyDirect_kennedy
 
+# Number of desired points on circumference of minimum enclosing circle
+NUM_POINTS = 10
+
+# Number of iterations needed for Welzl's Algorithm, multiplied by the number of points
+# TODO: Find optimal iteration factor / formula w.r.t. number of input points
+ITERATION_FACTOR = 10
+
+# Custom log level used for debug purposes
 LEVEL = 25
 
+# TODO: Wrap in a try-except block in a function to avoid spaghetti coding
+#  and handle potential connection errors
 CLIENT = CosmosClient.from_connection_string(os.environ['AzureCosmosDBConnectionString'])
 database_name = 'sensors'
 DATABASE = CLIENT.get_database_client(database_name)
@@ -25,40 +53,50 @@ LOGGER = logging.getLogger('log')
 
 def main(mytimer: func.TimerRequest):
     LOGGER.setLevel(LEVEL)
+
+    # Get current time for timestamp-based query filter
     current_time = datetime.now()
     prev_time = current_time - timedelta(hours=2)
     converted_time = datetime.timestamp(prev_time)
     LOGGER.log(LEVEL, "Time: {}".format(converted_time))
 
-    # TODO: Create aggregation list for coordinates retrieved from query
-
-    # TODO: Query Cosmos DB with timestamp clause, limit clause, and (if possible) a distance clause
-    #   NOTE: This may later be done via a stored proc on the Cosmos DB emulator
+    # Query Cosmos DB with timestamp clause
+    # NOTE: This may later be done via a stored proc on the Cosmos DB emulator
+    # TODO: Consider sensor readings as criteria for query, need significant readings
     query_str = "SELECT * FROM data r WHERE r._ts >= @time"
 
-    # read_results = list(CONTAINER.read_all_items(max_item_count=20))
     query_results = list(CONTAINER.query_items(query=query_str, parameters=[{"name": "@time", "value": converted_time}],
                                                enable_cross_partition_query=True))
     LOGGER.log(LEVEL, "LENGTH: {}".format(len(query_results)))
 
+    # Create aggregation set for coordinates retrieved from query
+    # A set prevents duplicate coordinates from being stored, which would impede
+    # the performance of Welzl's Algorithm
     coordinates = set()
     for doc in query_results:
+        # TODO: Check that `doc` has the below keys before accessing, may cause KeyError otherwise
         point_list = doc['geometry']['coordinates']
         point = (point_list[0], point_list[1])
-        point_str = "{},{}".format(point_list[0], point_list[1])
-        # LOGGER.log(LEVEL, "{}: {}".format(doc['id'], point_list))
-
         coordinates.add(point)
 
-    LOGGER.log(LEVEL, coordinates)
+    # Check if there are any coordinates from recent sensor readings of significance
+    if len(coordinates) == 0:
+        LOGGER.log(LEVEL, "No coordinates retrieved")
+        return
+    LOGGER.log(LEVEL, "Sensor coordinates retrieved: {}".format(coordinates))
 
-    # TODO: Apply Welzl's Algo to parsed coordinates
+    # Apply Welzl's Algo to parsed coordinates
     coord_list = list(coordinates)
-    max_iterations = len(coord_list) * 10
+    max_iterations = len(coord_list) * ITERATION_FACTOR
     nsphere: NSphere = welzl(points=coord_list, maxiterations=max_iterations)
 
+    # Log coordinates and radius of minimum enclosing circle
     LOGGER.log(LEVEL, "Center: {}, Radius: {}".format(nsphere.center, nsphere.sqradius))
 
-    # TODO: Obtain coordinates and radius of minimum enclosing circle
+    # Send out points along circumference of minimum enclosing circle
+    for bearing in range(0, 360, 360 // NUM_POINTS):
+        lat, lon, _ = vincentyDirect_kennedy(nsphere.center[0], nsphere.center[1], 1000 * (nsphere.sqradius ** 0.5),
+                                             bearing)
+        LOGGER.log(LEVEL, "Coordinates on Circumference: ({}, {})".format(lat, lon))
 
-    # TODO: Send out points along circumference
+    # TODO: Send out coordinates on circumference to IoT Hub drones via direct method, will need IotHubRegistry
