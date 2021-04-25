141217cc8f840f828593d7ee8caf43d687ce4380
Athreya Murali <34428511+DevByAthi@users.noreply.github.com>
2021-04-25 01:56:08 -0400

    Changed logging method

--- FireFlyFunctions/parse_actuator_data/__init__.py
+++ FireFlyFunctions/parse_actuator_data/__init__.py
@@ -4,11 +4,15 @@ from datetime import datetime, timedelta
 
 import azure.functions as func
 import geojson
+import pygeodesy as pyg
 from azure.cosmos import CosmosClient
 
 import numpy as np
 
 from ..shared_code.shared_utils import dict_to_str
+from ..shared_code.welzl import welzl, NSphere
+
+LEVEL = 25
 
 CLIENT = CosmosClient.from_connection_string(os.environ['AzureCosmosDBConnectionString'])
 database_name = 'sensors'
@@ -16,12 +20,15 @@ DATABASE = CLIENT.get_database_client(database_name)
 container_name = 'data'
 CONTAINER = DATABASE.get_container_client(container_name)
 
+LOGGER = logging.getLogger('log')
+
 
 def main(mytimer: func.TimerRequest):
+    LOGGER.setLevel(LEVEL)
     current_time = datetime.now()
-    prev_time = current_time - timedelta(minutes=20)
+    prev_time = current_time - timedelta(hours=2)
     converted_time = datetime.timestamp(prev_time)
-    logging.info("Time: {}".format(converted_time))
+    LOGGER.log(LEVEL, "Time: {}".format(converted_time))
 
     # TODO: Create aggregation list for coordinates retrieved from query
 
@@ -32,17 +39,18 @@ def main(mytimer: func.TimerRequest):
     # read_results = list(CONTAINER.read_all_items(max_item_count=20))
     query_results = list(CONTAINER.query_items(query=query_str, parameters=[{"name": "@time", "value": converted_time}],
                                                enable_cross_partition_query=True))
-    print("LENGTH: {}".format(len(query_results)))
+    LOGGER.log(LEVEL, "LENGTH: {}".format(len(query_results)))
 
-    coordinates = dict()
+    coordinates = set()
     for doc in query_results:
         point_list = doc['geometry']['coordinates']
+        point = (point_list[0], point_list[1])
         point_str = "{},{}".format(point_list[0], point_list[1])
-        print("{}: {}".format(doc['id'], point_list))
-        point = geojson.Point((point_list[0], point_list[1]))
-        coordinates[point_str] = point
+        # LOGGER.log(LEVEL, "{}: {}".format(doc['id'], point_list))
 
-    print(coordinates)
+        coordinates.add(point)
+
+    LOGGER.log(LEVEL, coordinates)
 
     # TODO: Apply Welzl's Algo to parsed coordinates
 
