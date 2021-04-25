bc8bd9bdb1eb79ff671c122e3637e0da92763326
Athreya Murali <34428511+DevByAthi@users.noreply.github.com>
2021-04-24 21:41:41 -0400

    Updated TODOs

--- FireFlyFunctions/parse_actuator_data/__init__.py
+++ FireFlyFunctions/parse_actuator_data/__init__.py
@@ -7,7 +7,19 @@ from azure.cosmos import CosmosClient
 CLIENT = CosmosClient.from_connection_string(os.environ['AzureCosmosDBConnectionString'])
 
 
+# TODO: Use Timer trigger instead of IoT Hub trigger
 def main(documents: func.DocumentList):
+
+    # TODO: Create aggregation list for coordinates retrieved from query
+
+    # TODO: Query Cosmos DB with timestamp clause, limit clause, and (if possible) a distance clause
+    #   NOTE: This may later be done via a stored proc on the Cosmos DB emulator
+
+    # TODO: Apply Welzl's Algo to parsed coordinates
+
+    # TODO: Obtain coordinates and radius of minimum enclosing circle
+
+    # TODO: Send out points along circumference
     if documents:
         logging.info('Document id: %s', documents[0]['id'])
         logging.info("Documents List length: {}".format(len(documents)))
