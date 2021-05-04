e78c6c9dcec02b55856306ce360099a09d1aa7d8
Athreya Murali <athreya.murali@gmail.com>
2021-05-03 20:56:23 -0400

    Added docstrings for utility functions

--- FireFlyFunctions/shared_code/shared_utils.py
+++ FireFlyFunctions/shared_code/shared_utils.py
@@ -1,17 +1,29 @@
+"""
+Miscellaneous Utility Functions
+
+by Athreya Murali
+
+Last Modified: 3 May 2021, 8:55 PM EDT
+
+"""
+
 import geojson
 
-
-
-
 def dict_to_str(d: dict) -> str:
+    """
+    Converts dictionary to a JSON-friendly string
+    :param d: input dictionary
+    :return: JSON-formatted string
+    """
     return str(d).replace("'", '"')
 
 
 def is_device_sensor(payload: geojson.Feature) -> bool:
     """
-    Assumes that payload is a geojson.Feature
-    :param payload:
-    :return:
+    Checks if given GeoJSON payload has a 'device_type' property, and checks that it is either a "sensor" or "actuator"
+    :param payload: GeoJSON payload
+    :return: boolean determining if payload is from a sensor or actuator
+    :exception: KeyError if 'device_type' is neither "sensor" nor "actuator"
     """
     if 'device_type' not in payload['properties']:
         return False
@@ -23,25 +35,8 @@ def is_device_sensor(payload: geojson.Feature) -> bool:
 
 
 def meets_device_format(d):
+    """
+    Returns boolean determining if given dictionary `d` or GeoJSON payload has a 'device_type' property
+    """
     return isinstance(d, geojson.Feature) and 'device_type' in d['properties']
 
-
-def get_always_fire(payload: dict) -> bool:
-    return True
-
-
-def get_mock_decision(payload: dict) -> bool:
-    """
-    A fake function for deciding on whether a wildfire is detected at a given sensor, using sensor's data payload and
-    preset threshold parameters. In reality, the linear combination of values would be decided by hyper-parameter tuning
-    via an ML algorithm.
-    :param payload:
-    :return:
-    """
-    THRESHOLD = 0.5
-    has_valid_data = 'temperature' in payload and 'humidity' in payload and \
-                     'carbon_monoxide' in payload and 'pm2_5' in payload
-    if has_valid_data:
-        return 0.2 * payload['temperature'] + 0.05 * payload['humidity'] + \
-               0.6 * payload['carbon_monoxide'] + 0.15 * payload['pm2_5'] > THRESHOLD
-    return False
